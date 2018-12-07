2029504644e08f968fe236045d674b624a802802
Clemclem74 <44778853+Clemclem74@users.noreply.github.com>
2018-12-07 16:40:46 +0100

    Update base_de_donnees.py

--- base_de_donnees.py
+++ base_de_donnees.py
@@ -1,10 +1,11 @@
-import dweepy
 import time
-import request
+import requests
 
-def StockDonnees(date, poubelle, temps):
+def stockDonnees(date, poubelle, temps):
 
 	#Ajoute (date, poubelle, temps), qui sont la date, le numero de la poubelle et le temps entre la poubelle pleine et la poubelle ramassee
 	#Dans la base de données pour permettre de faire des graphes (optimisation du dispositif)
 	
 	requests.post('https://docs.google.com/forms/d/e/1FAIpQLSei5ygsYkcSmhMgWVwfSCcaVQQ4UNeeNVcOqjRrgUHTWjVm6g/viewform',data = {'entry.679099635':date, 'entry.1606041453':poubelle, 'entry.2022895418':temps},verify=False)
+
+stockDonnees('22/11', '5', '6')
