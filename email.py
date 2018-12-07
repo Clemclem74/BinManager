e80f94fb8db18beec0266db8b41dace451957909
Clemclem74 <44778853+Clemclem74@users.noreply.github.com>
2018-12-07 13:47:31 +0100

    Update email.py

--- email.py
+++ email.py
@@ -1,10 +1,8 @@
 
 import smtplib
-#from email.MIMEMultipart import MIMEMultipart
-#from email.MIMEText import MIMEText
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
 
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 
 
 def envoieEmail():
