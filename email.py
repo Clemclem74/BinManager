16fc578284ba180bb8f0b1d5db03203b0417d289
Clemclem74 <44778853+Clemclem74@users.noreply.github.com>
2018-12-07 14:27:06 +0100

    Update email.py

--- email.py
+++ email.py
@@ -8,20 +8,15 @@ from email.MIMEText import MIMEText
 def envoieEmail():
 #Envoie un email a l organisme de ramassage des poubelles avec la localisation$
         msg = MIMEMultipart()
-        msg['From'] = 'camille.thomas42140@gmail.com'
+        msg['From'] = 'binmanager.maintenance@gmail.com'
         msg['To'] = 'camille.thomas42140@gmail.com'
         msg['Subject'] = 'Poubelle pleine'
-        message = 'La Poubelle numero x est pleine. Veuillez la collecter le p$
+        message = 'La Poubelle numero x est pleine. Veuillez la collecter le plus rapidement possible'
         msg.attach(MIMEText(message))
         mailserver = smtplib.SMTP('smtp.gmail.com', 587)
         mailserver.ehlo()
         mailserver.starttls()
         mailserver.ehlo()
-        mailserver.login('camille.thomas42140@gmail.com', 'cxmdp12021997')
-        mailserver.sendmail('camille.thomas42140@gmail.com', 'camille.thomas42$
+        mailserver.login('binmanager.maintenance@gmail.com', 'binmanagerIG3')
+        mailserver.sendmail('binmanager.maintenance@gmail.com', 'camille.thomas42@gmail.com', msg.as_string())
         mailserver.quit()
-
-
-
-envoieEmail()
-
