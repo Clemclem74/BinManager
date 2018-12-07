2cf7c29293a3f2afd787610d8867914e8b9f05df
Clemclem74 <44778853+Clemclem74@users.noreply.github.com>
2018-12-07 17:15:56 +0100

    Update base_de_donnees.py

--- base_de_donnees.py
+++ base_de_donnees.py
@@ -1,11 +1,18 @@
 import time
 import requests
+import urllib3
 
-def stockDonnees(date, poubelle, temps):
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+def stockDonneesPleinVide(date, poubelle, temps):
 	#Ajoute (date, poubelle, temps), qui sont la date, le numero de la poubelle et le temps entre la poubelle pleine et la poubelle ramassee
 	#Dans la base de données pour permettre de faire des graphes (optimisation du dispositif)
-	
 	requests.post('https://docs.google.com/forms/d/e/1FAIpQLSei5ygsYkcSmhMgWVwfSCcaVQQ4UNeeNVcOqjRrgUHTWjVm6g/viewform',data = {'entry.679099635':date, 'entry.1606041453':poubelle, 'entry.2022895418':temps},verify=False)
 
-stockDonnees('22/11', '5', '6')
+def stockDonneesVidePlein(mois, poubelle, nombre):
+	#Ajoute (mois, poubelle, nombre), qui sont le mois, le numero de la poubelle et le nombre de fois que la poubelle a ete pleine
+	#Dans la base de données pour permettre de faire des graphes (optimisation du dispositif)
+	requests.post('h',data = {'':mois, '':poubelle, '':nombre},verify=False)
+
+stockDonneesPleinVide('25/11','2','1')
+#stockDonneesVidePlein('janvier','2','11')
