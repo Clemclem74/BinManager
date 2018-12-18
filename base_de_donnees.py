9b627c3489e47b697d4554473e1b5860d1ca1e19
Clemclem74 <44778853+Clemclem74@users.noreply.github.com>
2018-12-18 15:16:23 +0100

    Update base_de_donnees.py

--- base_de_donnees.py
+++ base_de_donnees.py
@@ -4,7 +4,7 @@ import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-def stockDonneesPleinVide(date, poubelle, temps):
+def stockDonneesPleinVide(poubelle, temps):
 	#Ajoute (date, poubelle, temps), qui sont la date, le numero de la poubelle et le temps entre la poubelle pleine et la poubelle ramassee
 	#Dans la base de donnees pour permettre de faire des graphes (optimisation du dispositif)
 	requests.post('https://docs.google.com/forms/d/e/1FAIpQLSei5ygsYkcSmhMgWVwfSCcaVQQ4UNeeNVcOqjRrgUHTWjVm6g/formResponse', data = {'entry.679099635':date, 'entry.1606041453':poubelle, 'entry.2022895418':temps} ,verify=False)
